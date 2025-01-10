cedb635b16e0c845ce402a8a8cfe19d3301cb7b9
WilliamPoe <williamtpoe@gmail.com>
2025-01-10 11:21:05 -0600

    Working on second chart.

--- src/components/ExpenseBreakdown.jsx
+++ src/components/ExpenseBreakdown.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, {useEffect} from "react";
+import { Bar } from "react-chartjs-2";
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const ExpenseBreakdown = () => {
     return (
