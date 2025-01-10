29e550241733d0b439b473c8861719b8c524824a
WilliamPoe <williamtpoe@gmail.com>
2025-01-10 14:03:12 -0600

    Testing

---
+++ src/router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import MonthlyComparison from "./components/MonthlyComparison";
+
+export const = createBrowserRouter({
+    {path: "/", element: <App />},
+    {path: "/about", element <About/>},
+
+});
