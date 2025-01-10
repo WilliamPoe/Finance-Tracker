c0d85ecfb4c6bdb542f1d8e7de153ababd632748
WilliamPoe <williamtpoe@gmail.com>
2025-01-10 14:19:38 -0600

    Modified router file.

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import MonthlyComparison from "./components/MonthlyComparison";
 
 export const = createBrowserRouter({
-    {path: "/", element: <App />},
+    {path: "/", element: <Home />},
     {path: "/about", element <About/>},
 
 });
