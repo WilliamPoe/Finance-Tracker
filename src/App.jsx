d769861abff0b08fc5f34383a1bed4bb1ee89d26
WilliamPoe <williamtpoe@gmail.com>
2025-01-11 15:54:43 -0600

    Working on Navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MonthlyComparison from './components/MonthlyComparison'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div>
+      <Navbar/>
       <h1> Finance Tracker </h1>
       <p>Help keep track of your finances</p>
       <MonthlyComparison/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import "../styles/Navbar.css";
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav classname="nav">
+            <a href="/" classname="site-title"> Finance Tracker</a>
+            <ul>
+                <li>
+                    <a href="/expenses">Expenses</a>
+                </li>
+                <li>
+                    <a href="/stock">Stocks</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
